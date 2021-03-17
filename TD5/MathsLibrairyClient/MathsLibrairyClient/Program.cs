6d08a6eaea8b3b73d514d1976cf2f8fe49849132
Anthony Barna <anthony.barna@etu.univ-cotedazur.fr>
2021-03-17 15:55:34 +0100

    TD4

---
+++ TD5/MathsLibrairyClient/MathsLibrairyClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsLibrairyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathService.MathsOperationsClient service = new MathService.MathsOperationsClient();
+            Console.WriteLine(service.Multiply(4, 5));
+            Console.ReadLine();
+        }
+    }
+}
